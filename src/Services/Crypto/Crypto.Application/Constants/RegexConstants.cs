d7dafdb86bf76d57071afd494a53063aa2ba2094
bculo <culobozo@gmail.com>
2022-11-08 20:39:37 +0100

    Regex optimized

---
+++ src/Services/Crypto/Crypto.Application/Constants/RegexConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Crypto.Application.Constants
+{
+    public class RegexConstants
+    {
+        public static Regex SYMBOL = new Regex("^[a-zA-z]{1,15}$", 
+            RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture | RegexOptions.Compiled,
+            TimeSpan.FromSeconds(1)); 
+    }
+}
--- src/Services/Crypto/Crypto.Application/Modules/Crypto/Commands/UpdatePrice/UpdatePriceCommandValidator.cs
+++ src/Services/Crypto/Crypto.Application/Modules/Crypto/Commands/UpdatePrice/UpdatePriceCommandValidator.cs
@@ -1,10 +1,5 @@
-﻿using Crypto.Application.Constants.Regex;
+﻿using Crypto.Application.Constants;
 using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Crypto.Application.Modules.Crypto.Commands.UpdatePrice
 {
