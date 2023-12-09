ef6930192613ee5dab7c4bef034cd9e454659503
bculo <culobozo@gmail.com>
2023-12-09 22:18:03 +0100

    NSwag client generator added

---
+++ src/Tools/NSwag.Generator/Program.cs
@@ -0,0 +1,29 @@
+﻿using NSwag;
+using NSwag.CodeGeneration.CSharp;
+
+var filePath = Path.Combine("OpenApi", "keycloak-23.json");
+var serviceName = "KeycloakAdminApiClient";
+var nameSpace = "Keycloak.Admin.Client";
+
+if (!File.Exists(filePath))
+{
+    throw new FileNotFoundException();
+}
+
+var json = await File.ReadAllTextAsync(filePath);
+var document = await OpenApiDocument.FromJsonAsync(json);
+
+var settings = new CSharpClientGeneratorSettings
+{
+    ClassName = serviceName,
+    CSharpGeneratorSettings =
+    {
+        Namespace = nameSpace,
+    },
+    UseBaseUrl = false
+};
+
+var generator = new CSharpClientGenerator(document, settings);
+var code = generator.GenerateFile();
+
+await File.WriteAllTextAsync("../NSwagGenerated.cs", code);
