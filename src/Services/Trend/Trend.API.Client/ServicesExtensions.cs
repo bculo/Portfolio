a2a530288cf42d338ae2830580689ede69ae0824
bculo <culobozo@gmail.com>
2023-12-20 19:51:29 +0100

    feat: add trend api client that uses refit http client factory

---
+++ src/Services/Trend/Trend.API.Client/ServicesExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Contrib.WaitAndRetry;
+using Polly.Extensions.Http;
+using Refit;
+using Trend.API.Client.Services;
+
+namespace Trend.API.Client;
+
+public static class ServicesExtensions
+{
+    public static void ConfigureRefitClients(this IServiceCollection services, Uri baseUrl)
+    {
+        var clientBuilderINewsApi = services
+            .AddRefitClient<INewsApi>()
+            .ConfigureHttpClient(c => c.BaseAddress = baseUrl)
+            .AddPolicyHandler(
+                HttpPolicyExtensions
+                    .HandleTransientHttpError()
+                    .WaitAndRetryAsync(
+                        Backoff.DecorrelatedJitterBackoffV2(
+                            TimeSpan.FromSeconds(0.5),
+                            3)));
+
+        var clientBuilderISearchWordApi = services
+            .AddRefitClient<ISearchWordApi>()
+            .ConfigureHttpClient(c => c.BaseAddress = baseUrl)
+            .AddPolicyHandler(
+                HttpPolicyExtensions
+                    .HandleTransientHttpError()
+                    .WaitAndRetryAsync(
+                        Backoff.DecorrelatedJitterBackoffV2(
+                            TimeSpan.FromSeconds(0.5),
+                            3)));
+
+        var clientBuilderISyncApi = services
+            .AddRefitClient<ISyncApi>()
+            .ConfigureHttpClient(c => c.BaseAddress = baseUrl)
+            .AddPolicyHandler(
+                HttpPolicyExtensions
+                    .HandleTransientHttpError()
+                    .WaitAndRetryAsync(
+                        Backoff.DecorrelatedJitterBackoffV2(
+                            TimeSpan.FromSeconds(0.5),
+                            3)));
+    }
+}
