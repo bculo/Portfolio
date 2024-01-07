ecb3cb69a328bd90a70ad9709bf16e5271bb003d
bculo <culobozo@gmail.com>
2024-01-07 09:55:05 +0100

    feat: add moving particles background

--- src/Web/admin-dashboard/apps/trend-dashboard/src/app/app.icons.ts
+++ src/Web/admin-dashboard/apps/trend-dashboard/src/app/app.icons.ts
@@ -6,6 +6,11 @@ import {
     heroPower 
 } from '@ng-icons/heroicons/outline';
 
+import {
+    MoveDirection,
+    OutMode,
+  } from "@tsparticles/engine";
+
 export const APP_ICONS = {
     heroNewspaper,
     heroHome,
@@ -13,3 +18,62 @@ export const APP_ICONS = {
     heroChatBubbleLeftEllipsis,
     heroPower
 }
+
+export const ANIMATED_BACKGROUND = {
+    background: {
+      color: {
+        value: "#111827",
+      },
+    },
+    fpsLimit: 120,
+    interactivity: {
+      modes: {
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      move: {
+        direction: MoveDirection.none,
+        enable: true,
+        outModes: {
+          default: OutMode.bounce,
+        },
+        random: false,
+        speed: 2,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 600,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: { min: 1, max: 3 },
+      },
+    },
+    detectRetina: true,
+  };
--- src/Web/admin-dashboard/apps/trend-dashboard/src/app/layouts/navigation-layout/navigation-layout.component.ts
+++ src/Web/admin-dashboard/apps/trend-dashboard/src/app/layouts/navigation-layout/navigation-layout.component.ts
@@ -1,24 +1,28 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgIconComponent } from '@ng-icons/core';
 import { NAVIGATION_ITEMS } from '../../app.routes';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthStore } from '../../store/auth-store';
 import { nameOf } from '../../shared/utilities/utilities';
-import { APP_ICONS } from '../../app.icons';
+import { ANIMATED_BACKGROUND, APP_ICONS } from '../../app.icons';
 import { KeycloakService } from '../../shared/services/keycloak.service';
+import { NgxParticlesModule, NgParticlesService } from "@tsparticles/angular";
+import { loadSlim } from "@tsparticles/slim"; 
 
 @Component({
   selector: 'admin-dashboard-navigation-layout',
   standalone: true,
-  imports: [CommonModule, NgIconComponent, RouterLink, RouterLinkActive],
+  imports: [CommonModule, NgIconComponent, RouterLink, RouterLinkActive, NgxParticlesModule],
   templateUrl: './navigation-layout.component.html',
   styleUrl: './navigation-layout.component.scss',
 })
-export class NavigationLayoutComponent {
+export class NavigationLayoutComponent implements OnInit {
   readonly items = [...NAVIGATION_ITEMS];
   readonly authStore = inject(AuthStore);
   readonly keycloak = inject(KeycloakService);
+  readonly particalService = inject(NgParticlesService);
+  readonly particleSettings = ANIMATED_BACKGROUND;
 
   isAuthenticated = this.authStore.isAuthenticated;
   userName = this.authStore.userName;
@@ -27,5 +31,11 @@ export class NavigationLayoutComponent {
 
   logout() {
     this.keycloak.logout();
+  }
+
+  ngOnInit(): void {
+    this.particalService.init(async (engine) => {
+      await loadSlim(engine)
+    });
   }
 }
